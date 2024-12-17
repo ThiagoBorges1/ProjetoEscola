036f980255bca527257228c552e0b8a32e22b1f7
danipla4853Y <141415786+danipla4853Y@users.noreply.github.com>
2024-12-16 21:26:38 -0300

    Create descricoes.py

---
+++ descricoes.py
@@ -0,0 +1,29 @@
+import random
+
+nomes = nomes = ["Alice","Bernardo","Clara","Daniel","Elisa","Felipe","Gabriel","Helena","Igor","Júlia","Kaique",
+                 "Lara","Marcelo","Nathália","Otávio","Paula","Renato","Sara","Tiago","Vanessa","Ana","Bruno","Carolina",
+                 "Diego","Eduardo","Fernanda","Gustavo","Heloísa","Isabela","João","Karla","Lucas","Mariana","Nicolas",
+                 "Olívia","Pedro","Rafaela","Samuel","Tânia","Ubirajara","Vitória","William","Ximena","Yara","Zé","Artur",
+                 "Bianca","Caio","Débora","Elias"
+]
+sobrenomes = ["Silva", "Santos", "Oliveira", "Souza", "Lima", "Pereira", "Ferreira", "Costa", "Almeida", "Nascimento",
+              "Andrade", "Monteiro", "Cardoso", "Ribeiro", "Carvalho", "Correia", "Martins", "Barbosa", "Rocha", "Dias",
+              "Gomes", "Moreira", "Mendes", "Vieira", "Cruz", "Ramos", "Teixeira", "Freitas", "Lopes", "Fernandes",
+              "Castro", "Araújo", "Melo", "Neves", "Pinheiro", "Reis", "Viana", "Soares", "Campos", "Sales", "Leite",
+              "Coelho", "Machado", "Farias", "Fonseca", "Pinto", "Barros", "Antunes", "Prado", "Borges", "Morais"]
+
+
+enderecos = ["Rua A", "Rua B", "Avenida Central", "Praça das Flores", "Travessa Nova", "Estrada Real", "Vila Verde",
+             "Condomínio Azul", "Residencial Primavera", "Beco do Sol", "Largo dos Sonhos", "Alameda Pôr do Sol", 
+             "Rua do Comércio", "Estrada da Serra", "Centro Comercial", "Vila Mariana", "Jardim das Palmeiras",
+             "Praça da Paz", "Rua da Alegria", "Avenida Paulista", "Rodovia do Sol", "Praça do Mercado", "Rua dos Artistas",
+             "Bairro Alto", "Travessa das Rosas", "Vila Nova", "Centro Histórico", "Praça do Relógio", "Avenida Brasil",
+             "Largo do Carmo", "Vila Velha", "Jardim Europa", "Estrada Velha", "Centro Sul", "Rua Boa Vista", 
+             "Praça São João", "Avenida Atlântica", "Bairro Planalto", "Vila Esperança", "Praça das Nações", 
+             "Centro Oeste", "Travessa das Árvores", "Largo do Amor", "Jardim América", "Rua do Campo", "Condomínio Verde",
+             "Estrada Nova", "Residencial Harmonia", "Praça da Liberdade", "Rua do Porto"]
+
+filiacoes = ["Pai", "Mãe", "Tio", "Tia", "Avô", "Avó", "Tutor", "Tutora", "Guardião", "Guardião Legal"]
+
+registrosAcademicos = [f"RA{random.randint(1000000, 9999999)}" for i in range(50)]
+senhas = [f"senha{random.randint(1000, 9999)}" for _ in range(50)]
